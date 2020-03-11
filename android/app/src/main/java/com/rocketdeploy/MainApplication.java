46b69b5e6c2e714a65d8e947d7d67650238d1578
danileao <dani.leao89@gmail.com>
2020-03-11 15:56:56 -0300

    AppCenter

--- android/app/src/main/java/com/rocketdeploy/MainApplication.java
+++ android/app/src/main/java/com/rocketdeploy/MainApplication.java
@@ -20,13 +20,17 @@ public class MainApplication extends Application implements ReactApplication {
           return BuildConfig.DEBUG;
         }
 
+         @Override
+        protected String getJSBundleFile() {
+            return CodePush.getJSBundleFile();
+        }
+
         @Override
         protected List<ReactPackage> getPackages() {
           @SuppressWarnings("UnnecessaryLocalVariable")
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
-          packages.add(new CodePush(BuildConfig.CODEPUSH_KEY, getApplicationContext(), BuildConfig.DEBUG ));
           return packages;
         }
 
